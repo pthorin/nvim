79b5b7ce1e70f690e095ec264661e82b76d4904a
Peter Thorin <pthorin@volvocars.com>
2024-10-17 21:17:25 +0200

    yo

--- init.lua
+++ init.lua
@@ -103,9 +103,11 @@ vim.keymap.set('n', '<leader>sc', '<cmd>/^(<<<<<<<|>>>>>>>|=======)<CR>', { desc
 
 -- copy / paste
 vim.keymap.set('n', '<leader>y', '"+y', { desc = '[Y]ank to clipboard' })
-vim.keymap.set('v', '<leader>y', '"+y', { desc = '[Y]ank to clipboard' })
 vim.keymap.set('n', '<leader>p', '"+p', { desc = '[P]aste from clipboard' })
 vim.keymap.set('n', '<leader>P', '"+P', { desc = '[P]aste from clipboard' })
+vim.keymap.set('v', '<leader>y', '"+y', { desc = 'Visual [Y]ank to clipboard' })
+vim.keymap.set('v', '<leader>p', '"+p', { desc = 'Visual [P]aste from clipboard' })
+vim.keymap.set('v', '<leader>P', '"+P', { desc = 'Visual [P]aste from clipboard' })
 vim.keymap.set('n', 'Y', 'y$', { desc = 'Yank to end of line' })
 
 -- Diagnostic keymaps
@@ -233,18 +235,18 @@ require('lazy').setup({
       require('which-key').setup()
 
       -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+      require('which-key').add {
+        { '<leader>c', desc = '[C]ode', hidden = true },
+        { '<leader>d', desc = '[D]ocument', hidden = true },
+        { '<leader>r', desc = '[R]ename', hidden = true },
+        { '<leader>s', desc = '[S]earch', hidden = true },
+        { '<leader>w', desc = '[W]orkspace', hidden = true },
+        { '<leader>t', desc = '[T]oggle', hidden = true },
+        { '<leader>h', desc = 'Git [H]unk', hidden = true },
       }
       -- visual mode
-      require('which-key').register({
-        ['<leader>h'] = { 'Git [H]unk' },
+      require('which-key').add({
+        { '<leader>h', desc = 'Git [H]unk' },
       }, { mode = 'v' })
     end,
   },
@@ -368,6 +370,7 @@ require('lazy').setup({
       { 'williamboman/mason.nvim', config = true }, -- NOTE: Must be loaded before dependants
       'williamboman/mason-lspconfig.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
+      'nvim-java/nvim-java',
 
       -- Useful status updates for LSP.
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
@@ -430,6 +433,7 @@ require('lazy').setup({
           -- Jump to the implementation of the word under your cursor.
           --  Useful when your language has ways of declaring types without an actual implementation.
           map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+          map('gi', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
 
           -- Jump to the type of the word under your cursor.
           --  Useful when you're not sure what type a variable is and you want to see
@@ -628,8 +632,9 @@ require('lazy').setup({
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
+        javascript = { { 'prettierd', 'prettier' } },
         java = { 'google-java-format' },
+        -- yaml = { 'prettier' },
       },
     },
   },
