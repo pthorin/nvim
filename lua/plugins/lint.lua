004328a53fc50811ea4d2b304918fcdb15c61127
Peter Thorin <pthorin@volvocars.com>
2025-12-17 15:54:33 +0100

    feat: add keymap for linting

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -2,12 +2,15 @@ return {
   'mfussenegger/nvim-lint',
   config = function()
     require('lint').linters_by_ft = {
-      yaml = { 'vacuum' },
+      yaml = { 'vacuum', 'redocly' },
     }
     vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
       callback = function()
         require('lint').try_lint()
       end,
     })
+    vim.keymap.set('n', '<leader>l', function()
+      require('lint').try_lint()
+    end, { desc = 'Lint current buffer' })
   end,
 }
